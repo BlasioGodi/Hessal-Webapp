0d8e59ced8726ea2d72c1eb5103b434bc8ea63e8
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-12-15 13:12:45 +0300

    MVC Blog setup complete

--- HessalWebapp/HessalWebapp/Services/BlogPostService.cs
+++ HessalWebapp/HessalWebapp/Services/BlogPostService.cs
@@ -1,16 +1,23 @@
-﻿using HessalWebapp.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HessalWebapp.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HessalWebapp.Services
 {
     public class BlogPostService:PageModel
     {
-        public List<BlogPost> GetBlogPosts()
+        // Replace List<BlogPost> with your actual data source
+        private readonly List<BlogPost> _blogPosts;
+
+        //Constructor to Initialize Blog Post Service
+        public BlogPostService()
         {
-            // Logic to retrieve blog posts from a data source
-            return new List<BlogPost>
-        {
-            new BlogPost { Id = 1, Title = "LPG", Content = "Lorem ipsum...", Date = DateTime.Now },
+            // Initialize your blog posts data
+            _blogPosts = new List<BlogPost>
+            {
+                new BlogPost { Id = 1, Title = "LPG", Content = "Lorem ipsum...", Date = DateTime.Now },
             new BlogPost { Id = 2, Title = "LPG Production", Content = "Dolor sit amet...", Date = DateTime.Now.AddDays(-1) },
             new BlogPost { Id = 3, Title = "Gas Leak Detection", Content = "Dolor sit amet...", Date = DateTime.Now.AddDays(-1) },
             new BlogPost { Id = 4, Title = "Fire Safety", Content = "Dolor sit amet...", Date = DateTime.Now.AddDays(-1) },
@@ -19,7 +26,21 @@ namespace HessalWebapp.Services
             new BlogPost { Id = 7, Title = "Non-destructive Testing", Content = "Dolor sit amet...", Date = DateTime.Now.AddDays(-1) },
             new BlogPost { Id = 8, Title = "Earthing", Content = "Dolor sit amet...", Date = DateTime.Now.AddDays(-1) },
             new BlogPost { Id = 9, Title = "Hello World Blog", Content = "Dolor sit amet...", Date = DateTime.Now.AddDays(-1) }
-        };
+                // Add more blog posts as needed
+            };
+        }
+
+        public List<BlogPost> GetBlogPosts()
+        {
+            // Logic to retrieve blog posts from a data source
+            return _blogPosts;
+        }
+        public BlogPost GetBlogPostById(int id)
+        {
+            return _blogPosts.FirstOrDefault(post => post.Id == id);
         }
     }
 }
+
+
+
